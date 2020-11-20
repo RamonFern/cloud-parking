d989e2301851fb2ff94cd0d78e17f302c82974ea
Ramon Fernandes <ramonfernandes9@gmail.com>
2020-11-20 00:01:31 -0300

    deletar pelo id funcionando

--- src/main/java/br/com/rf/parking/estacionamento/controller/EstacionamentoController.java
+++ src/main/java/br/com/rf/parking/estacionamento/controller/EstacionamentoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,12 @@ public class EstacionamentoController {
 		var estacionamento = estacionamentoService.create(estacionamentoCreate);
 		var result = estacionamentoMapper.toEstacionamentoDTO(estacionamento);
 		return ResponseEntity.status(HttpStatus.CREATED).body(result);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity delete(@PathVariable String id) {
+		estacionamentoService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/br/com/rf/parking/estacionamento/service/EstacionamentoService.java
+++ src/main/java/br/com/rf/parking/estacionamento/service/EstacionamentoService.java
@@ -13,38 +13,44 @@ import br.com.rf.parking.estacionamento.models.Estacionamento;
 
 @Service
 public class EstacionamentoService {
-	
+
 	private static Map<String, Estacionamento> estacionamentoMap = new HashMap<String, Estacionamento>();
-	
+
 	static {
 		var id = getUUID();
 		var id1 = getUUID();
-		
-		Estacionamento estacionamento = new Estacionamento(id, "DDD-7878", "SC", "PALIO","PRATA");
-		Estacionamento estacionamento1 = new Estacionamento(id1, "DHG-8776", "MA", "CORSA","AZUL");
-		
-		
+
+		Estacionamento estacionamento = new Estacionamento(id, "DDD-7878", "SC", "PALIO", "PRATA");
+		Estacionamento estacionamento1 = new Estacionamento(id1, "DHG-8776", "MA", "CORSA", "AZUL");
+
 		estacionamentoMap.put(id, estacionamento);
 		estacionamentoMap.put(id1, estacionamento1);
 	}
-	public List<Estacionamento> findAll(){
+
+	public List<Estacionamento> findAll() {
 		return estacionamentoMap.values().stream().collect(Collectors.toList());
 	}
 
 	private static String getUUID() {
 		return UUID.randomUUID().toString().replace("-", "");
 	}
-	
+
 	public Estacionamento findById(String id) {
-        return estacionamentoMap.get(id);
-    }
-	
+		return estacionamentoMap.get(id);
+	}
+
 	public Estacionamento create(Estacionamento estacionamentoCreate) {
-        String uuid = getUUID();
-        estacionamentoCreate.setId(uuid);
-        estacionamentoCreate.setDataEntrada(LocalDateTime.now());
-        estacionamentoMap.put(uuid, estacionamentoCreate);
-        return estacionamentoCreate;
-    }
+		String uuid = getUUID();
+		estacionamentoCreate.setId(uuid);
+		estacionamentoCreate.setDataEntrada(LocalDateTime.now());
+		estacionamentoMap.put(uuid, estacionamentoCreate);
+		return estacionamentoCreate;
+	}
+
+	public void delete(String id) {
+		findById(id);
+		estacionamentoMap.remove(id);
+
+	}
 
 }
